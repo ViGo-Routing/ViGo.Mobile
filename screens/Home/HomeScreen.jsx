f166d220044223a8664499072092b2808aaaffce
Hoang Tran <66261053+zunpeter@users.noreply.github.com>
2023-06-06 14:38:50 +0700

    Apply new Component for HomeScreen
     CustomButton and PromotionCardSwiper

--- screens/Home/HomeScreen.jsx
+++ screens/Home/HomeScreen.jsx
@@ -1,31 +1,55 @@
 import React from 'react'
-import { StyleSheet, View, TouchableOpacity } from 'react-native'
-import { Ionicons } from '@expo/vector-icons'
+import { StyleSheet, View, ScrollView } from 'react-native'
+import { themeColors } from '../../assets/theme/index.jsx'
+import { useNavigation } from '@react-navigation/native'
+// IMPORT COMPONENTS
 import BottomNavigationBar from '../../components/NavBar/BottomNavigationBar.jsx'
 import HomeHeader from '../../components/Header/HomeHeader.jsx'
-import { themeColors } from '../../assets/theme/index.jsx'
-import LinkIcon from '../../components/Icon/LinkIcon.jsx'
-import { SvgUri } from 'react-native-svg';
-import { useNavigation } from '@react-navigation/native'
+import CustomButton from '../../components/Button/CustomButton.jsx'
+import PromotionCardSwiper from '../../components/Card/PromotionCardSwiper.jsx'
+
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <View style={styles.header}><HomeHeader title="Home" /></View>
-      <View style={styles.body}>
-        <View style={styles.iconRow}>
-          <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-            <SvgUri
-              width="100%"
-              height="100%"
-              uri=""
-            />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate('Promotion')}>
-            <Ionicons name="pricetags" size={40} color={themeColors.primary} />
-          </TouchableOpacity></View>
-      </View>
+      <ScrollView style={styles.body}>
+        <CustomButton
+          buttons={[
+            {
+              key: 'bike',
+              screen: 'BikeBooking',
+              icon: require('../../assets/icons/vigobike.png'),
+            },
+            {
+              key: 'car',
+              screen: 'CarBooking',
+              icon: require('../../assets/icons/vigocar.png'),
+            },
+          ]}
+        />
+        <PromotionCardSwiper
+          items={[
+            {
+              imageSource: require('../../assets/images/login.png'),
+              title: 'Title 1',
+              description: 'Description 1',
+            },
+            {
+              imageSource: require('../../assets/images/registration.png'),
+              title: 'Title 2',
+              description: 'Description 2',
+            },
+            {
+              imageSource: require('../../assets/images/registration.png'),
+              title: 'Title 3',
+              description: 'Description 3',
+            },
+          ]}
+          layout={'default'}
+        />
+      </ScrollView>
       <View style={styles.footer}><BottomNavigationBar /></View>
     </View>
   );
@@ -35,14 +59,28 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column', // inner items will be added vertically
     flexGrow: 1,            // all the available vertical space will be occupied by it
-    justifyContent: 'space-between' // will create the gutter between body and footer
+    justifyContent: 'space-between', // will create the gutter between body and footer
+    backgroundColor: themeColors.linear
+  },
+  imgae: {
+    width: 40,
+    height: 40,
+
   },
   body: {
     flex: 1,
   },
   iconRow: {
+    paddingTop: 10,
     flexDirection: 'row',
-  }
+    padding: 10,
+  },
+  box: {
+    borderRadius: 15,
+    backgroundColor: themeColors.primary,
+    padding: 5,
+    marginHorizontal: 5,
+  },
 });
 
 export default HomeScreen;
