3ee7cd0212d644a035f05f6bd0af888dada14836
Hoang Tran <hoangtran080700@gmail.com>
2023-07-03 23:06:07 +0700

    Update InputCard.jsx
    with google auto complete

--- components/Card/InputCard.jsx
+++ components/Card/InputCard.jsx
@@ -1,89 +1,154 @@
-import { Ionicons } from '@expo/vector-icons';
-import React, { useState } from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
-import { themeColors } from '../../assets/theme';
-import axios from 'axios';
+import { Ionicons } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const InputCard = () => {
-    const [startStation, setStartStation] = useState(null);
-    const [endStation, setEndStation] = useState(null);
+  const [startStation, setStartStation] = useState(null);
+  const [endStation, setEndStation] = useState(null);
 
-    // axios.get(`https://vigo-api.azurewebsites.net/api/Station/${data.startStationId}`, {
-    //     headers: {
-    //         Authorization: `Bearer ${token}`,
-    //         'Content-Type': 'application/json-patch+json'
-    //     }
-    // })
-    //     .then(response => {
-    //         setStartStation(response.name)
-    //         console.log('API Station Response:', response.data);
-    //         // You can update your component state or perform any other necessary actions with the response data
-    //     })
-    //     .catch(error => {
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [pickupPosition, setPickupPosition] = useState(null);
+  const [destinationPosition, setDestinationPosition] = useState(null);
 
-    //         console.log('API Station Error:', error);
-    //         // You can display an error message or perform any other necessary error handling
-    //     });
-    // axios.get(`https://vigo-api.azurewebsites.net/api/Station/${data.endStationId}`, {
-    //     headers: {
-    //         Authorization: `Bearer ${token}`,
-    //         'Content-Type': 'application/json-patch+json'
-    //     }
-    // })
-    //     .then(response => {
-    //         setEndStation(response.name)
-    //         // Handle the response data
-    //         console.log('API Station Response:', response.data);
-    //         // You can update your component state or perform any other necessary actions with the response data
-    //     })
-    //     .catch(error => {
-    //         // Handle the error
-    //         console.log('API Station Error:', error);
-    //         // You can display an error message or perform any other necessary error handling
-    //     });
+  const handlePlaceSelection = (details) => {
+    console.log("Father detail", details);
+    setSelectedPlace(details);
+    // Do something with the selected place details in the father components
+  };
+  const handlePickupPlaceSelection = (details) => {
+    setPickupPosition(details);
+  };
 
-    return (
-        <View style={styles.card}>
-            <View style={styles.row}>
-                <Ionicons name="trail-sign" size={20} color={themeColors.primary} />
-                <TextInput style={styles.input1} placeholder={startStation} />
-            </View>
-            <View style={styles.row}>
-                <Ionicons name="navigate-circle" size={20} color={themeColors.primary} />
-                <TextInput style={styles.input} placeholder={endStation} />
-            </View>
-        </View>
-    );
+  const handleDestinationPlaceSelection = (details) => {
+    setDestinationPosition(details);
+  };
+
+  const handleContinueButtonPress = (response) => {
+    console.log(savedPickDetails);
+
+    const requestData = {
+      // Request body data
+      name: "App Booking",
+      distance: 15,
+      duration: 15,
+      status: "ACTIVE",
+      routineType: "RANDOMLY",
+      routeType: "SPECIFIC_ROUTE_SPECIFIC_TIME",
+      startStation: {
+        longtitude: savedPickDetails.geometry.location.lng,
+        latitude: savedPickDetails.geometry.location.lat,
+        name: savedPickDetails.name,
+        address: savedPickDetails.formatted_address,
+      },
+      endStation: {
+        longtitude: savedDesDetails.geometry.location.lng,
+        latitude: savedDesDetails.geometry.location.lat,
+        name: savedDesDetails.name,
+        address: savedDesDetails.formatted_address,
+      },
+    };
+
+    console.log("send ", requestData);
+    response = createRoute(requestData);
+    if (response != null) {
+      navigation.navigate("BikeSettingSchedule");
+    }
+  };
+
+  return (
+    <View style={styles.card}>
+      <View style={styles.row}>
+        <Ionicons name="person-circle-outline" size={20} color="blue" />
+        {/* <TextInput
+          style={styles.input1}
+          placeholder="Chọn điểm bắt đầu"
+          pickupPositionDetail={pickupPosition}
+        /> */}
+        <GooglePlacesAutocomplete
+          placeholder="Điểm đón ..."
+          styles={{
+            textInput: {
+              fontSize: 16,
+            },
+          }}
+          onPress={handlePickupPlaceSelection}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          return
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: "AIzaSyCIYCycKF24mQXN1pJYFfCO-6azSETj_Qc",
+            language: "vn",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+      </View>
+      <View style={styles.separator} />
+      <View style={styles.row}>
+        <Ionicons name="compass-outline" size={18} color="orange" />
+        {/* <TextInput style={styles.input} placeholder="Chọn điểm kết thuc" destinationPositionDetail={destinationPosition} /> */}
+        <GooglePlacesAutocomplete
+          placeholder="Điểm đến ..."
+          styles={{
+            textInput: {
+              fontSize: 16,
+            },
+          }}
+          onPress={handleDestinationPlaceSelection}
+          returnKeyType={"search"}
+          fetchDetails={true}
+          return
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: "AIzaSyCIYCycKF24mQXN1pJYFfCO-6azSETj_Qc",
+            language: "vn",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    card: {
-        padding: 10,
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-        backgroundColor: '#fff',
-    },
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    input1: {
-        height: 40,
-        borderColor: 'gray',
-        paddingHorizontal: 10,
-        borderBottomWidth: 1,
-        flex: 1,
-    },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        paddingHorizontal: 10,
-    },
-
+  card: {
+    margin: 10,
+    padding: 10,
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    backgroundColor: "#fff",
+    width: "90%",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input1: {
+    height: 25,
+    borderColor: "gray",
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    flex: 1,
+  },
+  input: {
+    height: 25,
+    borderColor: "gray",
+    paddingHorizontal: 10,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "#ddd",
+    marginVertical: 10,
+  },
 });
 
 export default InputCard;
